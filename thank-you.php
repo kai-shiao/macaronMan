bb739a8012212b069bb8550ea1cfcd42630c08bb
Kai Shiao <kshiao13@gmail.com>
2016-03-21 21:05:27 -0700

    Added if-else statement to handle non-existence of form data from HTML page.

--- thank-you.php
+++ thank-you.php
@@ -8,7 +8,7 @@ is dedicated to producing in-house, high end, and premium quality macarons using
 <link rel="icon" type="image/x-icon" href="multimedia/beardedManIcon.ico"/>
 <link rel="shortcut icon" type="image/x-icon" href="multimedia/beardedManIcon.ico"/>
 <link href="http://localhost/macaronMan/css/thank-you/main.css" rel="stylesheet">
-<title>THANK YOU!</title>
+<title>FEEDBACK STATUS CONFIRMATION</title>
 </head>
 <body>
 <div id="wrapper">
@@ -22,7 +22,7 @@ is dedicated to producing in-house, high end, and premium quality macarons using
 		</ul>
 	</nav>
 
-<h1>THANK YOU FOR YOUR FEEDBACK!</h1>
+<h1>FEEDBACK STATUS CONFIRMATION</h1>
 </header>
 <?php
 $name=filter_input(INPUT_POST,"name");
@@ -31,74 +31,83 @@ $emailAddress=filter_input(INPUT_POST,"emailAddress");
 $contactNumber=filter_input(INPUT_POST,"contactNumber");
 $feedback=filter_input(INPUT_POST,"feedback");
 
-#Call the PHPMailer package available for download on GitHub and set up the necessary details to send a SMTP email to the shop's email account.
-require "phpmailer/PHPMailerAutoload.php";
+if (isset($name) && isset($capitalizedName) && isset($emailAddress) && isset($contactNumber) && isset($feedback)){
+	#Call the PHPMailer package available for download on GitHub and set up the necessary details to send a SMTP email to the shop's email account.
+	require "phpmailer/PHPMailerAutoload.php";
 
-$mail = new PHPMailer();
-$mail->IsSMTP();                                      	// Use SMTP
-$mail->Host = "";  										// Use your email provider's SMTP host. 
-$mail->SMTPAuth = true;                               	// SMTP authentication
-$mail->Username = "";          							// Use your own email account details to test this function
-$mail->Password = "";                      				// Password removed for security reasons.
-$mail->SMTPSecure = "";                            	
-$mail->Port = "";                                    	// Look up SMTP settings for your email provider.
-$mail->setFrom("from@example.com");
-$mail->addAddress("themacaronman@hotmail.com");     	// The shop is the recipient.
-$mail->isHTML(true);                                  	// Send a HTML email.
+	$mail = new PHPMailer();
+	$mail->IsSMTP();                                      	// Use SMTP
+	$mail->Host = "";  										// Use your email provider's SMTP host. 
+	$mail->SMTPAuth = true;                               	// SMTP authentication
+	$mail->Username = "";          							// Use your own email account details to test this function
+	$mail->Password = "";                      				// Password removed for security reasons.
+	$mail->SMTPSecure = "";                            	
+	$mail->Port = "";                                    	// Look up SMTP settings for your email provider.
+	$mail->setFrom("from@example.com");
+	$mail->addAddress("themacaronman@hotmail.com");     	// The shop is the recipient.
+	$mail->isHTML(true);                                  	// Send a HTML email.
 
-$mail->Subject="RECEIPT OF FEEDBACK FROM $capitalizedName (EMAIL ADDRESS: $emailAddress)";
+	$mail->Subject="RECEIPT OF FEEDBACK FROM $capitalizedName (EMAIL ADDRESS: $emailAddress)";
 
-//In my PHP.ini file, I set the default timezone to 'Europe/London'.
-$date=new DateTime();
-$currentDayOfTheWeek=$date->format("l");
-$currentMonth=$date->format("F");
-$currentDayOfTheMonth=$date->format("j");
-$currentYear=$date->format("Y")+60; //I added 60 to the current year to get 2076, in line with the fictional year used in the website.
-$currentHour=$date->format("G"); //24-hour time format.
-$currentMinute=$date->format("i");
+	//In my PHP.ini file, I set the default timezone to 'Europe/London'.
+	$date=new DateTime();
+	$currentDayOfTheWeek=$date->format("l");
+	$currentMonth=$date->format("F");
+	$currentDayOfTheMonth=$date->format("j");
+	$currentYear=$date->format("Y")+60; //I added 60 to the current year to get 2076, in line with the fictional year used in the website.
+	$currentHour=$date->format("G"); //24-hour time format.
+	$currentMinute=$date->format("i");
 
-$mail->Body="<p style=\"font-weight:bold; text-transform:uppercase;\">$name (email address: <span style=\"text-transform:lowercase;\">$emailAddress</span>";
-$mail->Body.=" ; contact number: $contactNumber) sent the following feedback to the macaron man on";
-$mail->Body.=" $currentDayOfTheWeek, $currentMonth $currentDayOfTheMonth, $currentYear at $currentHour:$currentMinute.</p>";
-$mail->Body.="<p style=\"border-top: 3px solid rgb(0,0,0);\"></p>";
-$mail->Body.="<p style=\"font-weight:bold; text-transform:lowercase;\">$feedback</p>";
-$mail->Body.="<p style=\"border-top: 3px solid rgb(0,0,0)\";></p>";
+	$mail->Body="<p style=\"font-weight:bold; text-transform:uppercase;\">$name (email address: <span style=\"text-transform:lowercase;\">$emailAddress</span>";
+	$mail->Body.=" ; contact number: $contactNumber) sent the following feedback to the macaron man on";
+	$mail->Body.=" $currentDayOfTheWeek, $currentMonth $currentDayOfTheMonth, $currentYear at $currentHour:$currentMinute.</p>";
+	$mail->Body.="<p style=\"border-top: 3px solid rgb(0,0,0);\"></p>";
+	$mail->Body.="<p style=\"font-weight:bold; text-transform:lowercase;\">$feedback</p>";
+	$mail->Body.="<p style=\"border-top: 3px solid rgb(0,0,0)\";></p>";
 
-if(!$mail->send()) {
-    print <<<HERE
-	<section> 
-			<p>Sorry, your feedback was not received via email by THE MACARON MAN.</p>
-			<p>The following error occurred: <span>$mail->ErrorInfo</span></p>
-			<p>Please report this to the webmaster as soon as possible.</p>
-	</section>
+	if(!$mail->send()) {
+		print <<<HERE
+		<section> 
+				<p>Sorry, your feedback was not received via email by THE MACARON MAN.</p>
+				<p>The following error occurred: <span>$mail->ErrorInfo</span></p>
+				<p>Please report this to the webmaster as soon as possible.</p>
+		</section>
 HERE;
+	} else {
+		print <<<HERE
+		<section>
+				<p>Your feedback was successfully received via email by THE MACARON MAN on <span> $currentDayOfTheWeek, $currentMonth $currentDayOfTheMonth, 
+				$currentYear at $currentHour:$currentMinute.</span></p>
+				<p>Please print the following feedback submitted for your own records:</p>
+				<section id="contents">
+						<p>NAME:</p> 
+						<p>$capitalizedName</p>
+						<p>EMAIL ADDRESS:</p> 
+						<p>$emailAddress</p>
+						<p>MESSAGE:</p>
+						<p>$feedback</p>
+				</section>
+		</section>
+		<section>
+			<p>THANK YOU FOR YOUR FEEDBACK!</p>
+			<p>GIVEN THE HIGH VOLUME OF EMAILS RECEIVED, IT MAY TAKE AS LONG AS <span>THREE (3) BUSINESS DAYS</span> TO RECEIVE A RESPONSE FROM THE MACARON MAN.</p>
+		</section>
+HERE;
+	}
 } else {
 	print <<<HERE
-	<section>
-			<p>Your feedback was successfully received via email by THE MACARON MAN on <span> $currentDayOfTheWeek, $currentMonth $currentDayOfTheMonth, 
-			$currentYear at $currentHour:$currentMinute.</span></p>
-			<p>Please print the following feedback submitted for your own records:</p>
-			<section id="contents">
-					<p>NAME:</p> 
-					<p>$capitalizedName</p>
-					<p>EMAIL ADDRESS:</p> 
-					<p>$emailAddress</p>
-					<p>MESSAGE:</p>
-					<p>$feedback</p>
-			</section>
-	</section>
+	<p>NO CONTACT INFORMATION WAS RECEIVED FROM <span>contact.html</span></p>
 HERE;
 }
-
+$date=new DateTime();	
+$currentYear=$date->format("Y")+60; 
 //Dynamically generated year display.
+
 print <<<HERE
-<section>
-		<p>THANK YOU FOR YOUR FEEDBACK!</p>
-		<p>GIVEN THE HIGH VOLUME OF EMAILS RECEIVED, IT MAY TAKE AS LONG AS <span>THREE (3) BUSINESS DAYS</span> TO RECEIVE A RESPONSE FROM THE MACARON MAN.</p>
-</section>
 <footer> 
-&copy; $currentYear. THE MACARON MAN. ALL RIGHTS RESERVED.
-		<p id="inside">CONTACT THE WEBMASTER AT <span>WEBMASTER@THEMACARONMAN.COM</span> TO REPORT ANY WEB BROWSING PROBLEMS AND SUGGESTIONS.</p>
+		<p>&copy; $currentYear. THE MACARON MAN. ALL RIGHTS RESERVED.</p>
+		<p>FLAGSHIP STORE ADDRESS: 888 AVENIDA DE LA REP&Uacute;BLICA POPULAR FUENTES, BONAVENTURA.</p>
+		<p>CONTACT THE WEBMASTER AT <span>WEBMASTER@THEMACARONMAN.COM</span> TO REPORT ANY WEB BROWSING PROBLEMS AND SUGGESTIONS.</p>
 </footer>
 HERE;
 ?>
